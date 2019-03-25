8d36b4847f0a85d75d7ea2e1e305938da939d860
ghr85 <ghrenfrew@gmail.com>
2019-03-25 16:02:46 +0000

    Tests designed for calculator class, failing

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestCalculator {
+    private Calculator calculator;
+
+    @Before
+    public void before(){
+    this.calculator = new Calculator();
+    }
+    @Test
+    //add,subtract,multiply,divide
+    public void canAdd(){
+    assertEquals(5,calculator.add(2,3));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(1, calculator.subtract(3,2));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(3,calculator.divide(12,4));
+    }
+    @Test
+    public void canMultiply(){
+        assertEquals(12, calculator.multiply(3,4));
+    }
 }
